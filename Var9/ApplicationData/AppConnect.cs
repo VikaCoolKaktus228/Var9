70872c543e835e1ba95ef8362132d117c18d0157
ssss <delovayakolbasa321@gmail.com>
2024-10-25 16:20:23 +0300

    Добавьте файлы проекта.

---
+++ Var9/ApplicationData/AppConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var9.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static Entities ModelFish;
+    }
+}
